11a81886d6d608bd34751b0e09598def4000e2a9
wmc41 <wmc41@users.noreply.github.com>
2021-03-03 22:06:39 -0500

    CardProspector
    Added CardProspector Script to Prefab card and removed the old card script.

--- Assets/__Scripts/Prospector.cs
+++ Assets/__Scripts/Prospector.cs
@@ -5,9 +5,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 
-public class Prospector : MonoBehaviour {
+public class Prospector : MonoBehaviour
+{
 
-	static public Prospector 	S;
+	static public Prospector S;
 
 	[Header("Set in Inspector")]
 	public TextAsset deckXML;
@@ -16,20 +17,35 @@ public class Prospector : MonoBehaviour {
 
 
 	[Header("Set Dynamically")]
-	public Deck	deck;
+	public Deck deck;
 	public Layout layout;
+	public List<CardProspector> drawPile;
 
-
-	void Awake(){
+	void Awake()
+	{
 		S = this;
 	}
 
-	void Start() {
-		deck = GetComponent<Deck> ();
-		deck.InitDeck (deckXML.text);
+	void Start()
+	{
+		deck = GetComponent<Deck>();
+		deck.InitDeck(deckXML.text);
 		Deck.Shuffle(ref deck.cards); // This shuffles the deck
 		layout = GetComponent<Layout>(); // Get the Layout component
 		layout.ReadLayout(layoutXML.text); // Pass LayoutXML to it
+
+		drawPile = ConvertListCardsToListCardProspectors(deck.cards);
 	}
 
+	List<CardProspector> ConvertListCardsToListCardProspectors(List<Card> lCD)
+	{
+		List<CardProspector> lCP = new List<CardProspector>();
+		CardProspector tCP;
+		foreach (Card tCD in lCD)
+		{
+			tCP = tCD as CardProspector; // a
+			lCP.Add(tCP);
+		}
+		return (lCP);
+	}
 }
